9b067e7fe0004108bb4e913d3b17e976f27da9bf
Boshi <boshi2pan@gmail.com>
2022-02-24 14:15:27 +0000

    Cors problem fix on login and register
    adding annotation CrosOriginal to Control

--- src/main/java/com/cs7cs3/JourneySharing/controllers/LoginController.java
+++ src/main/java/com/cs7cs3/JourneySharing/controllers/LoginController.java
@@ -9,13 +9,10 @@ import com.cs7cs3.JourneySharing.utils.Utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/login")
 public class LoginController {
 
